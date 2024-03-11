609c2f3f168debc003e7658337882ef6c1225cde
Umudov Nurlan <nurlanmu@code.local>
2024-03-11 17:27:45 +0400

    add practise

---
+++ Generics-Practise/IntList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Practise
+{
+    internal class IntList
+    {
+        private int[] intArray;
+
+        public IntList()
+        {
+            intArray = Array.Empty<int>();               //new int[0];
+        }
+
+
+        public void Add(int number)
+        {
+            Array.Resize(ref intArray, intArray.Length + 1);
+            intArray[intArray.Length - 1] = number;
+        }
+
+        public int[] GetAll()
+        {
+            return intArray;
+        }
+    }
+}
---
+++ Generics-Practise/Program.cs
@@ -0,0 +1,121 @@
+﻿
+
+
+
+using Generics_Practise;
+
+//IntList datas = new IntList();
+
+//datas.Add(1);
+//datas.Add(2);
+//datas.Add(3);
+//datas.Add(4);
+//datas.Add(5);
+
+
+//int[] result = datas.GetAll();
+
+
+//foreach (var item in result)
+//{
+
+//    //Console.WriteLine(item);
+//}
+
+
+
+
+
+
+
+
+
+//StringList stringList = new StringList();
+
+//stringList.Add("salam");
+//stringList.Add("sagol");
+
+//var result2 = stringList.GetAll();
+
+//foreach ( var item in result2)
+//{
+////    Console.WriteLine(item);
+//}
+
+
+
+
+
+
+
+DataList<int> datas = new DataList<int>();
+
+
+datas.Add(1);
+datas.Add(2);
+datas.Add(3);
+
+
+var result = datas.GetAll();
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
+
+
+DataList<string> dataList = new DataList<string>();
+
+dataList.Add("SALAM");
+dataList.Add("sagol");
+dataList.Add("HER VAXTIN XEYIR");
+
+var result2 = dataList.GetAll();
+
+foreach (var item in result2)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
+
+DataList<bool> dataList1 = new DataList<bool>();
+
+
+dataList1.Add(true);
+dataList1.Add(false);
+dataList1.Add(false);
+dataList1.Add(true);
+
+var result3 = dataList1.GetAll();
+
+foreach (var item in result3)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
+
+
+
+DataList<Book> books = new DataList<Book>();
+
+books.Add(new Book { Id = 1, Name = "TEST1" });
+books.Add(new Book { Id = 2, Name = "TEST2" });
+books.Add(new Book { Id = 3, Name = "Test3" });
+
+var result5 = books.GetAll();
+
+foreach (var item in result5)
+{
+    Console.WriteLine(item.Name);
+}
+
